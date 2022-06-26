41071a710a0fabd130f1a0988530589438c1cc61
lautarotranquilli <lautitranquilli9@outlook.es>
2022-06-26 16:50:18 -0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Matchbox.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
